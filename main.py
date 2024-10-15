7035006413a4bfde589fb1b0f1f0d47fa73a66ff
GovindAudichya1234 <govind.audichya@hubblehox.com>
2024-10-15 11:53:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -816,7 +816,7 @@ class DriveService:
         SCOPES = ['https://www.googleapis.com/auth/drive']
         
         # Retrieve the decryption key from environment variables (set in GitHub Secrets)
-        decryption_key = secrets['DK']['value']
+        decryption_key = os.getenv('DK')
         if not decryption_key:
             st.error("Decryption key not found in environment.")
             raise ValueError("Decryption key not found.")
